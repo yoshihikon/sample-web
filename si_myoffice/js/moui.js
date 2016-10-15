ee8937c10bd8966bcbf8ee741f384a037c8dfff8
yoshihikon <yoshihiko@yBook.local>
2016-10-15 11:13:56 +0900

    commit

--- si_myoffice/js/moui.js
+++ si_myoffice/js/moui.js
@@ -1,15 +1,43 @@
+"use strict"
+
 /*
  * My OFFICE moui.js
  * 2016/9/29
  */
 
-"use strict;"
+/* 実行 */
+$(function(){
+  MOUI.setAccordion();
+});
 
-// Class ------------------------------------------------
+/* クラス */
 function MOUI() {
 };
 
-// Implementation ---------------------------------------
 MOUI.someMethod = function(){
   
+}
+
+MOUI.setAccordion = function(){
+  
+  //init
+  $(".moui-accordion-title").addClass("moui-close");
+  $(".moui-accordion-contents").hide();
+  
+  //action
+  $(".moui-accordion-title").on("click", function(){
+    
+    if($(this).hasClass("moui-close")){
+      $(this).next().slideDown("fast");
+      
+      $(this).removeClass("moui-close");
+      $(this).addClass("moui-open");
+    }else{
+      $(this).next().slideUp("fast");
+      
+      $(this).removeClass("moui-open");
+      $(this).addClass("moui-close");
+    }
+    
+  });
 }
